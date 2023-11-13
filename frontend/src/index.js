90dfa92cd7ff56456758a08ab47136bcd8a34868
Neel <nkachalia1@gmail.com>
2023-11-13 10:33:31 -0500

    frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,33 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store';
+import csrfFetch from './store/csrf';
+import * as sessionActions from './store/session';
 
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+}
+
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
+} else {
+  renderApplication();
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,6 +44,8 @@ const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
